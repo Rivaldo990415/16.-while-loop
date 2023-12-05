a4e7b514811ec7d42b8e2ac2075d5ecec8f6b6c1
SBICZA01\A253638 <rivaldo.mkhola@standardbank.co.za>
2023-12-05 15:08:57 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16.while_loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int index = 0;
+
+            Console.WriteLine("This is the while loop");
+            while(index <= 5)
+            {
+                Console.WriteLine(index);
+                index++;
+            }
+            Console.WriteLine("This is a do while loop");
+
+            int index2 = 0;
+            do
+            {
+                Console.WriteLine("index2");
+                index2++;
+            }
+            while (index2 <= 5);
+
+            //freeze console
+            Console.ReadLine();
+        }
+    }
+}
